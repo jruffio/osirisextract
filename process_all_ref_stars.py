9072eeb54f752fb4ac90f83b2703404d7c21b4df
Jean-Baptiste Ruffio <jruffio@sh-ln04.stanford.edu>
2019-03-21 19:59:21 -0700

    tmp

--- process_all_ref_stars.py
+++ process_all_ref_stars.py
@@ -11,16 +11,17 @@ print("coucou")
 #os.system("module load python/3.6.1")
 
 
-OSIRISDATA = "/data/osiris_data/"
-# OSIRISDATA = "/scratch/groups/bmacint/osiris_data/"
+#OSIRISDATA = "/data/osiris_data/"
+OSIRISDATA = "/scratch/groups/bmacint/osiris_data/"
 # IFSfilter = "Jbb"
-# IFSfilter = "Hbb"
-IFSfilter = "Kbb"
+IFSfilter = "Hbb"
+#IFSfilter = "Kbb"
 # refstar_name = "HD_210501"
 refstar_name = "HR_8799"
 cutoff = 5
 
-IFSfilter_list = ["Jbb","Hbb","Kbb"]
+IFSfilter_list = ["Hbb"]#["Jbb","Hbb","Kbb"]
+#refstar_name_list = ["HD_210501"]
 refstar_name_list = ["HD_210501","HIP_1123","HR_8799","BD+14_4774","HD_7215","HIP_18717"]
 
 for IFSfilter in IFSfilter_list:
@@ -34,10 +35,10 @@ for IFSfilter in IFSfilter_list:
 
         for filename in filelist:
             print(filename)
-            # continue
+            #continue
 
             inputdir = os.path.dirname(filename)
-            script = "~/OSIRIS/osirisextract/reduce_HPFonly_diagcov.py"
+            script = "~/OSIRIS/osirisextract/charac_reference_stars.py"
 
             logdir = os.path.join(inputdir,"sherlock_logs")
             if not os.path.exists(logdir):
@@ -49,7 +50,7 @@ for IFSfilter in IFSfilter_list:
             if 1 and len(glob.glob(os.path.join(filename.replace(".fits","_cutoff{0}_transmission.fits".format(cutoff))))) >= 1:
                 print("skip"+filename)
                 continue
-            continue
+            #continue
             numthreads = 16
             bsub_str= 'sbatch --partition=hns,owners,iric --qos=normal --time=2-0:00:00 --mem=60G --output='+outfile+' --error='+errfile+' --nodes=1 --ntasks-per-node='+str(numthreads)+' --mail-type=END,FAIL,BEGIN --mail-user=jruffio@stanford.edu --wrap="python3 ' + script
             params = ' {0} {1} {2} {3} {4} {5}"'.format(OSIRISDATA,filename,refstar_name,IFSfilter,numthreads,cutoff)
@@ -66,5 +67,5 @@ for IFSfilter in IFSfilter_list:
             print(bsub_out)
             #jobid_list.append(bsub_out.split(" ")[-1].strip('\n'))
 
-            exit()
+            #exit()
             time.sleep(2)
