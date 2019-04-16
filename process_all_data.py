3ca599e43140bc2589d45231df4a30f442fda6b2
Jean-Baptiste Ruffio <jruffio@sh-ln03.stanford.edu>
2019-04-15 21:53:30 -0700

    coucou

--- process_all_data.py
+++ process_all_data.py
@@ -20,16 +20,16 @@ year = "*"
 #year = "20150720"
 reductionname = "reduced_jb"
 #filenamefilter = "s*_a*001_tlc_Kbb_020.fits"
-filenamefilter = "s*Kbb_020.fits"
+filenamefilter = "s*Kbb*.fits"
 #filenamefilter = "s101104_a03*001_Hbb_020.fits"
 planet_search = 1 # If True, pixel resolution entire FOV, otherwise centroid
 debug_paras = 0 # If True, fast reduction
-planet_model_string = "'model'"#"'CO'"#"'model'"
+planet_model_string = "'CO'"#"'model'"#"'CO'"#"'CO2'"#H20#CH4#"'model'"
 
 filelist = glob.glob(os.path.join(OSIRISDATA,foldername,year,reductionname,filenamefilter))
 filelist.sort()
-for filename in filelist:
-    #print(filename)
+for fileid, filename in enumerate(filelist):
+    print(fileid,filename)
     #continue
 
     inputdir = os.path.dirname(filename)
@@ -47,14 +47,14 @@ for filename in filelist:
     outfile = os.path.join(logdir,now+os.path.basename(script).replace(".py","")+"_"+os.path.basename(filename).replace(".fits","{0}.out".format(planet_search)))
     errfile = os.path.join(logdir,now+os.path.basename(script).replace(".py","")+"_"+os.path.basename(filename).replace(".fits","{0}.err".format(planet_search)))
 
-    outputdir = os.path.join(inputdir,"sherlock","20190309_HPF_only")
-    if 0 and len(glob.glob(os.path.join(outputdir,os.path.basename(filename).replace(".fits","_outputHPF_cutoff40_sherlock_v1_search.fits")))) >= 1:
+    outputdir = os.path.join(inputdir,"sherlock","20190414_HPF_only")
+    if 1 and len(glob.glob(os.path.join(outputdir,os.path.basename(filename).replace(".fits","_outputHPF_cutoff40_sherlock_v1_search.fits")))) >= 1:
         #print("skip"+filename)
         continue
-    print(filename)
+    #print(filename)
     #continue
-    numthreads = 16
-    bsub_str= 'sbatch --partition=hns,owners,iric --qos=normal --time=1-00:00:00 --mem=60G --output='+outfile+' --error='+errfile+' --nodes=1 --ntasks-per-node='+str(numthreads)+' --mail-type=END,FAIL,BEGIN --mail-user=jruffio@stanford.edu --wrap="python3 ' + script
+    numthreads = 10
+    bsub_str= 'sbatch --partition=hns,owners,iric --qos=normal --time=0-15:00:00 --mem=20G --output='+outfile+' --error='+errfile+' --nodes=1 --ntasks-per-node='+str(numthreads)+' --mail-type=END,FAIL,BEGIN --mail-user=jruffio@stanford.edu --wrap="python3 ' + script
     params = ' {0} {1} {2} {3} {4} {5} {6} {7}"'.format(OSIRISDATA,inputdir,outputdir,filename,numthreads,planet_search,planet_model_string,debug_paras)
 
     print(bsub_str+params)
@@ -62,6 +62,6 @@ for filename in filelist:
     bsub_out = os.popen(bsub_str + params).read()
     print(bsub_out)
     #jobid_list.append(bsub_out.split(" ")[-1].strip('\n'))
-
-    exit()
+    
+    #exit()
     time.sleep(2)
