b186f57796d8a77ff672810c5042ec036667e8a3
Jean-Baptiste Ruffio <jruffio@sh-ln06.stanford.edu>
2019-07-16 10:57:17 -0700

    update

--- mcmc_orbits.py
+++ mcmc_orbits.py
@@ -12,10 +12,11 @@ print("coucou")
 
 
 OSIRISDATA = "/scratch/groups/bmacint/osiris_data/"
-uservs = False
-for planet in ["b","c","d","e"]:
+uservs = True
+for planet in ["b","c"]:#,"d","e"]:
+#for planet in ["bc"]:
     astrometry_DATADIR = os.path.join(OSIRISDATA,"astrometry")
-    if uservs and (planet == "b" or planet =="c"):
+    if uservs and (planet == "b" or planet =="c" or planet =="bc"):
         filename = "{0}/HR8799{1}_rvs.csv".format(astrometry_DATADIR,planet)
     else:
         filename = "{0}/HR8799{1}.csv".format(astrometry_DATADIR,planet)
@@ -24,16 +25,16 @@ for planet in ["b","c","d","e"]:
 
     logdir = os.path.join(astrometry_DATADIR,"logs")
     now = "{date:%Y%m%d_%H%M%S}_".format(date=datetime.datetime.now())
-    outfile = os.path.join(logdir,now+os.path.basename(script).replace(".py","")+"_"+os.path.basename(filename).replace(".fits",".out"))
-    errfile = os.path.join(logdir,now+os.path.basename(script).replace(".py","")+"_"+os.path.basename(filename).replace(".fits",".err"))
+    outfile = os.path.join(logdir,now+os.path.basename(script).replace(".py","")+"_"+os.path.basename(filename).replace(".csv",".out"))
+    errfile = os.path.join(logdir,now+os.path.basename(script).replace(".py","")+"_"+os.path.basename(filename).replace(".csv",".err"))
 
-    num_temps = 20
-    num_walkers = 100
-    total_orbits = 20000 # number of steps x number of walkers (at lowest temperature)
-    burn_steps = 100 # steps to burn in per walker
-    thin = 2 # only save every 2nd step
-    numthreads = 10
-    suffix = "sherlock"
+    num_temps = 16
+    num_walkers = 512
+    total_orbits = 40000000 # number of steps x number of walkers (at lowest temperature)
+    burn_steps = 0 # steps to burn in per walker
+    thin = 50 # only save every 2nd step
+    numthreads = 16
+    suffix = "sherlock_ptemceefix_{0}_{1}_{2}_{3}_{4}".format(num_temps,num_walkers,total_orbits//num_walkers,thin,uservs)
 
     # osiris_data_dir = sys.argv[1]
     # filename = sys.argv[2]
@@ -46,10 +47,12 @@ for planet in ["b","c","d","e"]:
     # num_threads = int(sys.argv[9]) # or a different number if you prefer
     # suffix = sys.argv[10]
     bsub_str= 'sbatch --partition=hns,owners,iric --qos=normal --time=2-00:00:00 --mem=20G --output='+outfile+' --error='+errfile+' --nodes=1 --ntasks-per-node='+str(numthreads)+' --mail-type=END,FAIL,BEGIN --mail-user=jruffio@stanford.edu --wrap="python3 ' + script
+    #bsub_str= 'sbatch --partition=normal          --qos=long   --time=7-00:00:00 --mem=40G --output='+outfile+' --error='+errfile+' --nodes=1 --ntasks-per-node='+str(numthreads)+' --mail-type=END,FAIL,BEGIN --mail-user=jruffio@stanford.edu --wrap="python3 ' + script
     params = ' {0} {1} {2} {3} {4} {5} {6} {7} {8} {9}"'.format(OSIRISDATA,filename,planet,num_temps,num_walkers,total_orbits,burn_steps,thin,numthreads,suffix)
 
     print(bsub_str+params)
-    exit()
+    #continue
+    #exit()
     bsub_out = os.popen(bsub_str + params).read()
     print(bsub_out)
     #jobid_list.append(bsub_out.split(" ")[-1].strip('\n'))
